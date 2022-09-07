a12394934f9aad989e22fd258347f1617f105099
adityayadav71 <adityayadav1088@gmail.com>
2022-09-07 15:03:43 +0530

    add join contest and isRegistered function

--- server/controllers/contestController.js
+++ server/controllers/contestController.js
@@ -1,6 +1,7 @@
 const catchAsync = require("../utils/catchAsync");
 const Contest = require("./../models/contestModel");
 const JWT = require("jsonwebtoken");
+const AppError = require("../utils/appError");
 
 exports.upcoming = catchAsync(async (req, res, next) => {
   const upcomingContests = await Contest.find({ status: "UPCOMING" });
@@ -12,8 +13,10 @@ exports.upcoming = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.retrieve = catchAsync(async (req, res, next) => {
-  const contests = await Contest.find();
+exports.getContestDetails = catchAsync(async (req, res, next) => {
+  const contests = await Contest.find()
+    .populate("contestants")
+    .populate("joined");
   const results = await Contest.find().count();
   res.status(200).json({
     status: "success",
@@ -49,12 +52,39 @@ exports.cancelContest = catchAsync(async (req, res, next) => {
 exports.register = catchAsync(async (req, res, next) => {
   const user = JWT.verify(req.cookies.jwt, process.env.JWT_SECRET);
   const contest = await Contest.findOneAndUpdate(
-    { _id: req.body._id },
-    { $addToSet: { participants: user.userdata.email } }
+    { _id: req.body.contestId },
+    { $addToSet: { contestants: user.userdata._id } }
   );
   res.status(200).json({
     status: "success",
     registered: contest.registered,
     message: "User registered successfully",
+  });
+});
+
+exports.isRegistered = catchAsync(async (req, res, next) => {
+  const user = JWT.verify(req.cookies.jwt, process.env.JWT_SECRET);
+  const contest = await Contest.findOne({ _id: req.body.contestId });
+  if (contest.contestants.includes(user.userdata._id)) next();
+  else {
+    return next(
+      new AppError(
+        "You have not registered for the contest yet. Please register first to join",
+        403
+      )
+    );
+  }
+});
+
+exports.join = catchAsync(async (req, res, next) => {
+  const user = JWT.verify(req.cookies.jwt, process.env.JWT_SECRET);
+  const contest = await Contest.findOneAndUpdate(
+    { _id: req.body.contestId },
+    { $addToSet: { joined: user.userdata._id } }
+  );
+  res.status(200).json({
+    status: "success",
+    attended: contest.attended,
+    message: "You have joined the contest",
   });
 });
