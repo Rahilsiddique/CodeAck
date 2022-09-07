b33b95d5f58ed2438d69d391bd961766c6b4f535
adityayadav71 <adityayadav1088@gmail.com>
2022-09-07 15:03:18 +0530

    function name change

--- server/routes/contestRoutes.js
+++ server/routes/contestRoutes.js
@@ -5,10 +5,11 @@ const router = express.Router();
 
 router.get("/upcoming", contestController.upcoming);
 router.post("/register", contestController.register);
+router.post("/join", contestController.isRegistered, contestController.join);
 
 router
   .route("/")
-  .get(contestController.retrieve)
+  .get(contestController.getContestDetails)
   .post(protectAdminRoutes.protect, contestController.createContest)
   .patch(protectAdminRoutes.protect, contestController.updateContestDetails)
   .delete(protectAdminRoutes.protect, contestController.cancelContest);
