eb4b3db651fb3994fcb5b5efebcd716f6806776b
adityayadav71 <adityayadav1088@gmail.com>
2022-09-07 15:03:00 +0530

    changed field names and types

--- server/models/contestModel.js
+++ server/models/contestModel.js
@@ -1,44 +1,56 @@
 const mongoose = require("mongoose");
 
-const contestSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Contest title is required"],
-    unique: [true, "This title has already been used"],
-  },
-  description: {
-    type: String,
-  },
-  winners: {
-    type: [String],
-  },
-  status: {
-    type: String,
-    enum: {
-      values: ["UPCOMING", "ONGOING", "COMPLETED"],
-      message:
-        "{VALUE} is not supported as contest status, try among UPCOMING, ONGOING, COMPLETED",
+const contestSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Contest title is required"],
+      unique: [true, "This title has already been used"],
     },
-    required: [true, "Contest status is required"],
+    description: String,
+    winners: [String],
+    status: {
+      type: String,
+      enum: {
+        values: ["UPCOMING", "ONGOING", "COMPLETED"],
+        message:
+          "{VALUE} is not supported as contest status, try among UPCOMING, ONGOING, COMPLETED",
+      },
+      required: [true, "Contest status is required"],
+    },
+    date: {
+      type: Date,
+      required: [true, "Date is required"],
+    },
+    duration: {
+      type: String,
+      required: [true, "Duration is required"],
+    },
+    problems: {
+      type: [String],
+      required: [true, "Problem IDs are required"],
+    },
+    contestants: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "users",
+      },
+    ],
+    joined: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "users",
+      },
+    ],
   },
-  date: {
-    type: Date,
-    required: [true, "Date is required"],
-  },
-  duration: {
-    type: String,
-    required: [true, "Duration is required"],
-  },
-  problems: {
-    type: [String],
-    required: [true, "Problem IDs are required"],
-  },
-  participants: [{ type: String, unique: true }],
-  joined: [{ type: String, unique: true }],
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 contestSchema.virtual("registered").get(function () {
-  return this.participants.length;
+  return this.contestants.length;
 });
 
 contestSchema.virtual("attended").get(function () {
