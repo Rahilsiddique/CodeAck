36877091aec48d5373f3acc9e3e01c4a1c2d9a39
adityayadav71 <adityayadav1088@gmail.com>
2022-10-04 06:55:15 +0530

    added .vscode to gitignore

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
- import MainPage from "./components/MainPage";
- import MainNav from "./components/MainPageContent/MainNav";
-import Profile from "./screen/Profile";
-import LandingPage from "./testing/LandingPage";
-import AdminProfile from './screen/AdminProfile'
-import Login from "./components/Authentication/Login";
 import MainPage from "./components/MainPage";
 import MainNav from "./components/MainPageContent/MainNav";
-import AdminProfile from "./screen/AdminProfile";
 import Profile from "./screen/Profile";
 import LandingPage from "./testing/LandingPage";
+import AdminProfile from "./screen/AdminProfile";
+import Login from "./components/Authentication/Login";
 
 function App() {
   return (
