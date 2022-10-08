da7c5870837ef266a3524a71359fc983374e5ffd
bloodmonk7 <rritikssingh@gmail.com>
2022-10-08 16:18:01 +0530

    Made navbar responsive issue resolved #9

--- client/src/components/LandingPageContent/LandingPageNavbar.js
+++ client/src/components/LandingPageContent/LandingPageNavbar.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import { FaAlignCenter, FaAlignJustify } from "react-icons/fa";
+import React, { useState } from 'react';
+import { FaAlignCenter, FaAlignJustify } from 'react-icons/fa';
 
 const LandingPageNavbar = () => {
-  const [navOn, setNavOn] = useState(false);
+  const [navOn, setNavOn] = useState(true);
   return (
     <div className="w-full top-0 z-30 scroll-smooth">
       <nav className="flex items-center justify-between flex-wrap bg-[#1D3557] p-6 scroll-smooth">
@@ -17,27 +17,35 @@ const LandingPageNavbar = () => {
         </div>
         <div
           className={`text-lg lg:w-auto justify-between px-10 lg:block ${
-            navOn && "hidden"
+            navOn && 'hidden'
           }`}
         >
-          <a
-            href="#home"
-            className="block mt-4 lg:inline-block lg:mt-0 text-ligthGreen hover:text-crimsonLike px-5 transition duration-300"
-          >
-            Home
-          </a>
-          <a
-            href="#about-us"
-            className="block mt-4 lg:inline-block lg:mt-0 text-ligthGreen hover:text-crimsonLike px-5 transition duration-300"
-          >
-            About Us
-          </a>
-          <a
-            href="#contact-us"
-            className="block mt-4 lg:inline-block lg:mt-0 text-ligthGreen hover:text-crimsonLike px-5 transition duration-300"
-          >
-            Contact Us
-          </a>
+          <ul className="flex flex-wrap justify-center items-center">
+            <li>
+              <a
+                href="#home"
+                className="block mt-4 lg:inline-block lg:mt-0 text-ligthGreen hover:text-crimsonLike px-5 transition duration-300"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="#about-us"
+                className="block mt-4 lg:inline-block lg:mt-0 text-ligthGreen hover:text-crimsonLike px-5 transition duration-300"
+              >
+                About Us
+              </a>
+            </li>
+            <li>
+              <a
+                href="#contact-us"
+                className="block mt-4 lg:inline-block lg:mt-0 text-ligthGreen hover:text-crimsonLike px-5 transition duration-300"
+              >
+                Contact Us
+              </a>
+            </li>
+          </ul>
         </div>
         <div className={`lg:hidden`}>
           <FaAlignJustify
