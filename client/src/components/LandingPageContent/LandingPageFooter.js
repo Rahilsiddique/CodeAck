a6343f5762ad6d8cdef45f3860e9c4c911ed099f
adityayadav71 <adityayadav1088@gmail.com>
2022-10-08 21:20:48 +0530

    replaced class with className

--- client/src/components/LandingPageContent/LandingPageFooter.js
+++ client/src/components/LandingPageContent/LandingPageFooter.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const LandingPageFooter = () => {
   return (
-    <footer class="p-2 bg-darkGreen shadow md:flex md:items-center md:justify-between md:p-2">
-      <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+    <footer className="p-2 bg-darkGreen shadow md:flex md:items-center md:justify-between md:p-2">
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
         © 2022{" "}
-        <a href="https://flowbite.com/" class="hover:underline">
+        <a href="https://flowbite.com/" className="hover:underline">
           codeAck™
         </a>
         . All Rights Reserved.
