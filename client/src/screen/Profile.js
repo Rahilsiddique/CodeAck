c64a16b8f6904d0667c315d69c5669f074d76262
Rahilsiddique <siddiquerahil19@gmail.com>
2022-10-25 22:29:00 +0530

    solved authentication error
    Signed-off-by: Rahilsiddique <siddiquerahil19@gmail.com>

--- client/src/screen/Profile.js
+++ client/src/screen/Profile.js
@@ -5,15 +5,12 @@ import { FaTimes } from "react-icons/fa";
 import { MainContext } from "../context/context";
 
 const Profile = () => {
-  const { setProfileEnter } = useContext(MainContext);
-  const [userData, setUserData] = useState(Cookies.get());
-  userData = jwtDecode(Cookies.get());
-  console.log(userData);
+  const { setProfileEnter, authData } = useContext(MainContext);
   return (
     <div className="border-2 border-whiteLike w-64 rounded-lg m-1 content-center flex flex-col absolute z-10 bg-ligthGreen top-0 right-0">
       <div className="flex justify-center">
         <img
-          src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
+          src={`${authData?.userdata.profilePicture}`}
           alt="ayo"
           className="rounded-full m-2 h-24 w-24 flexs justify-center"
         />
@@ -24,7 +21,7 @@ const Profile = () => {
           <FaTimes />
         </span>
       </div>
-      <div>Name</div>
+      <div>{authData?.userdata.username}</div>
       <div>Rank</div>
       <div>total no of contest attended</div>
     </div>
