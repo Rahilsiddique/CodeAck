f6ea3fe2f150b1975cbea5fbeacd4c5e09ebbcec
adityayadav71 <adityayadav1088@gmail.com>
2022-10-26 18:29:23 +0530

    fixed test changes

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ mongoose
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   if (process.env.NODE_ENV === "development")
-    console.log(`Testing pipeline ${port}`);
+    console.log(`App running on port ${port}`);
 });
 
 process.on("unhandledRejection", (err) => {
